2495e2abb00f1926e6d9fb55a73ec45da7ea86f8
Oum Derek <derekoum17@gmail.com>
2024-01-24 11:10:33 +0700

    header title

--- src/components/header/rootHeader.tsx
+++ src/components/header/rootHeader.tsx
@@ -3,11 +3,16 @@ import { CartButton } from "../button/cartButton";
 import { ProfileButton } from "../button/profileButton";
 import { ShopButton } from "../button/shopButton";
 import { HeaderSearch } from "./headerSearch";
+import { Link } from "react-router-dom";
 
 export const RootHeader = () => {
   return (
     <Header className="flex flex-row justify-center place-items-center gap-4 bg-indigo-950">
-      <h1 className="text-white text-sm">Products Store</h1>
+      <h1 className="text-white text-sm">
+        <Link to={"/"} className="text-white">
+          Products Store
+        </Link>
+      </h1>
       <HeaderSearch />
       <ShopButton />
       <ProfileButton />
